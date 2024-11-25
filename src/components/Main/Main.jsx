8d48ac817a3a6ab3b172568806ac7f1bb9909f1e
Tiffany Jones <tiffany1331jones@gmail.com>
2024-11-25 16:55:07 -0500

    Initial commit of Vite project

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,100 @@
+import React, { useContext } from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+import { Context } from '../../context/context'
+
+const Main = () => {
+
+    const { input,
+        setInput,
+        recentPrompt,
+        setRecentPrompt,
+        prevPrompt,
+        setPreviousPrompt,
+        onSent,
+        showResults,
+        setShowResults,
+        loading,
+        setLoading,
+        resultData,
+        setShowResultData } = useContext(Context)
+
+
+
+
+
+    return (
+        <div className='main'>
+            <div className="nav">
+                <p></p>
+                <img className="pics"src={assets.user_icon} alt="" />
+            </div>
+            <div className="main-container">
+                {!showResults
+                    ? <>
+                        <div className="greet">
+                            <p><span>Hello!</span></p>
+                            <p>How can I help you today?</p>
+                        </div>
+                        <div className="cards">
+                            <div className="card">
+                                <p>Suggest beautiful places to see on an upcoming road trip</p>
+                                <img src={assets.compass_icon} alt="" />
+
+                            </div>
+                            <div className="card">
+                                <p>Briefly summarize this concept: urban planning</p>
+                                <img src={assets.bulb_icon} alt="" />
+
+                            </div>
+                            <div className="card">
+                                <p>Brainstorm team bonding activities for our work retreat</p>
+                                <img src={assets.message_icon} alt="" />
+
+                            </div>
+                            <div className="card">
+                                <p>Improve the readability of the following code</p>
+                                <img src={assets.code_icon} alt="" />
+
+                            </div>
+                        </div>
+
+
+                    </>
+                    : <div className="result">
+                        <div className="result-title">
+                            <img src={assets.user_icon} alt="" />
+                            <p> {recentPrompt} </p>
+                        </div>
+                        <div className="result-data">
+                            <img src={assets.gemini_icon} alt="" />
+                            {loading ? <div className="loader">
+                                <hr />
+                                <hr />
+                                <hr />
+                            </div> : <p dangerouslySetInnerHTML={{ __html: resultData }} ></p>}
+
+
+                        </div>
+                    </div>
+                }
+
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input onChange={(e) => setInput(e.target.value)} value={input} type='text' placeholder='Enter prompt here' />
+                        <div>
+                            <img src={assets.gallery_icon} alt="" />
+                            <img src={assets.mic_icon} alt="" />
+                            <img onClick={() => onSent()} src={assets.send_icon} alt="" />
+                        </div>
+                    </div>
+                    <p className="bottom-info">
+                    Inspired by the design and functionality of Google's Gemini. We all make mistakes, so double-check your responses.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
